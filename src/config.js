7b5e3fa0e3a511396733188f6c2f3f411a21d941
xz131714 <zwl2247103169@gmail.com>
2025-04-30 12:36:04 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -133,7 +133,7 @@ const config = {
   //项目卡片 其中 字段"show"控制初始卡片的text是否展开
   projectcards:[ 
     {go:"🚀 前往",img:"https://alist.nn.ci/logo.svg",title:"小张的秘密基地",subtitle:"1,000 miles of wonder", text:"If you see this line, I've managed to get your attention.",url:"https://alist.xiaozhangya.xin",show:false},
-    {go:"🗂️ 前往",img:"/img/sunshine.jpg",title:"Project 2",subtitle:"2,000 miles of wonder", text:"If you see this line, I've managed to get your attention.",url:"https://xiaozhangya.vercel.app/public/MD/快速排序.md",show:false},
+    {go:"🗂️ 前往",img:"/img/sunshine.jpg",title:"Project 2",subtitle:"2,000 miles of wonder", text:"If you see this line, I've managed to get your attention.",url:"/MD/快速排序.md",show:false},
     // {go:"📝 前往",img:"/img/sunshine.jpg",title:"Project 3",subtitle:"3,000 miles of wonder", text:"If you see this line, I've managed to get your attention.",url:"xiaozhangya.vercel.app",show:false},
     // {go:"👍 前往",img:"/img/sunshine.jpg",title:"Project 4",subtitle:"4,000 miles of wonder", text:"If you see this line, I've managed to get your attention.",url:"xiaozhangya.vercel.app",show:false},
     // {go:"🗃 前往",img:"/img/sunshine.jpg",title:"Project 5",subtitle:"5,000 miles of wonder", text:"If you see this line, I've managed to get your attention.",url:"xiaozhangya.vercel.app",show:false},
